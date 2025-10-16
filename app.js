dcf86bb22b58090d386ec8a5ea1e7c1e735903b0
Luana598 <marianaluana598@gmail.com>
2025-10-15 22:15:37 -0300

    livros adicionados a tela principal, implementando filmes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const galeria = document.getElementById('bookGallery')
+const galeriaDeLivros = document.getElementById('bookGallery')
+const galeriaDeFilmes = document.getElementById('movieGallery')
 
 async function buscarLivros() {
     const url = `https://api.potterdb.com/v1/books`
@@ -15,27 +16,83 @@ async function exibirLivros() {
 
     const bookList = await buscarLivros()
 
-    //console.log(bookList.data['0'])
+    const books = bookList.data;
 
-    let array = bookList.data
-    console.log(array)
+    books.forEach(book => {
+
+        const card = document.createElement('div')
+        card.className = 'card'
 
 
-    while (array.lenght <= 7) {
-        const img = document.createElement("img")
-        img.src = url
-        galeria.appendChild(img)
-    }
-    
-    // bookList.forEach(data => {
-    //     const img = document.createElement("img")
-    //     img.src = url
-    //     galeria.appendChild(img)
-    //   })
+        const capa = document.createElement('img')
+        capa.src = book.attributes.cover
 
-    
+        const nome = document.createElement('p')
+        nome.textContent = book.attributes.title
 
+        const releaseDate = document.createElement('a')
+        releaseDate.textContent = book.attributes.release_date
+
+        const pageNumber = document.createElement('a')
+        pageNumber.textContent = `${book.attributes.pages} pages`
+
+        const buttonDetails = document.createElement('button')
+        buttonDetails.textContent = 'Show More'
+
+        card.appendChild(capa)
+        card.appendChild(nome)
+        card.appendChild(releaseDate)
+        card.appendChild(pageNumber)
+        card.appendChild(buttonDetails)
+        galeriaDeLivros.appendChild(card)
+
+    });
 }
 
+async function buscarFilmes() {
+    const url = `https://api.potterdb.com/v1/movies`
+    const response = await fetch(url)
+    const filmes = await response.json()
 
+    console.log(filmes)
+    return filmes
+}
+
+async function exibirFilmes() {
+
+    const movieList = await buscarFilmes()
+
+    const movies = movieList.data;
+
+    movies.forEach(movie => {
+
+        const card = document.createElement('div')
+        card.className = 'movieCard'
+
+        const poster = document.createElement('img')
+        poster.src = movie.attributes.poster
+
+        const nome = document.createElement('p')
+        nome.textContent = movie.attributes.title
+
+        const releaseDate = document.createElement('a')
+        releaseDate.textContent = movie.attributes.release_date
+
+        const runningTime = document.createElement('a')
+        runningTime.textContent = movie.attributes.running_time
+
+        const buttonDetails = document.createElement('button')
+        buttonDetails.textContent = 'Show More'
+
+        card.appendChild(poster)
+        card.appendChild(nome)
+        card.appendChild(releaseDate)
+        card.appendChild(runningTime)
+        card.appendChild(buttonDetails)
+        console.log(card)
+        galeriaDeFilmes.appendChild(card)
+
+    });
+}
+exibirFilmes()
 exibirLivros()
